fae68366e611677e89e1e18fb6b2ab2e8b7b7bb8
Ron Masas <ronmasas@gmail.com>
2022-02-26 07:35:15 +0200

    Update chrome-bandit.rb

--- chrome-bandit.rb
+++ chrome-bandit.rb
@@ -3,11 +3,12 @@ class ChromeBandit < Formula
     homepage "https://github.com/breakpointHQ/chrome-bandit"
     url "https://github.com/breakpointHQ/chrome-bandit/archive/refs/tags/v1.0-beta-0.2.tar.gz"
     version "v1.0-beta-0.2"
+    sha256 "32b922a404dff1613d2d4aab8de969a76fa2ab97f3b17090acc6ad11035a9524"
   
     def install
-      bin.install "chrome-bandit"
-      prefix.install Dir["commands/*"]
-      prefix.install Dir["resources/*"]
-      prefix.install Dir["utils/*"]
+      bin.install "bin/chrome-bandit"
+      prefix.install Dir["commands"]
+      prefix.install Dir["resources"]
+      prefix.install Dir["utils"]
     end
 end
