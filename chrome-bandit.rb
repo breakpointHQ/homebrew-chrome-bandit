a51b08bb2a8916fc5edf974eb7abbbc03a8ae460
Ron Masas <ronmasas@gmail.com>
2022-02-26 07:27:12 +0200

    Update chrome-bandit.rb

--- chrome-bandit.rb
+++ chrome-bandit.rb
@@ -1,9 +1,8 @@
 class ChromeBandit < Formula
     desc "Programmatically extract saved passwords from Chromium based browsers on macOS"
     homepage "https://github.com/breakpointHQ/chrome-bandit"
-    url "https://github.com/breakpointHQ/chrome-bandit/archive/refs/tags/v1.0-beta-0.1.tar.gz"
-    version "v1.0-beta-0.1"
-    sha256 "14b9803d14ac49270a24b8449a0959f6a346fbba9cf0bf2cb156428fa51229fa"
+    url "https://github.com/breakpointHQ/chrome-bandit/archive/refs/tags/v1.0-beta-0.2.tar.gz"
+    version "v1.0-beta-0.2"
   
     def install
       bin.install "chrome-bandit"
